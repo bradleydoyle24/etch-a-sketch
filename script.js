2e7fe94999a6bb0a74d190a66aa0dc346c167284
Bradley <bradleydoyle24@gmail.com>
2023-06-09 21:11:16 -0400

    Create pseudocode in script, create .pixel class in styles.css with a 1 pixel solid border

---
+++ script.js
@@ -0,0 +1,21 @@
+// Create a 16 x 16 square of divs
+
+// Create variable for container
+// Create variable for new div
+  // Modify CSS stylings for new div?
+// Create row/column of divs
+// Append to new div
+// Append to container
+
+const pixel = document.createElement('div');
+
+
+// Create a square div
+  // Height and width are ==.
+  // Must match width/height of container
+    // Width / 16 = length of 1 container
+    // Height / 16 = length of 1 container
+
+// Repeat 16x for row
+  // iterate through loop
+// Then repeat 16x for columns.
