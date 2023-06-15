6b103d762f1dba30a397eb32376b6ddb62bc6519
Bradley <bradleydoyle24@gmail.com>
2023-06-15 19:40:17 -0400

    Create function to turn pixel black when hovered over
    pixelBlack() uses a loop to add an event listener for 'mouseenter'.
    Changes the class to pixelBlack which only has a background color
    change to black as the rule.

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const row = document.createElement('div');
 row.style.display = 'flex';
 row.style.flexDirection = 'column';
 
-let number = 3;
-
+let number = 50;
 let pixel = [];
 let pixelRow = [];
 
@@ -21,20 +20,21 @@ console.log(`container height = ${contHeight}`);
 let pixelBorderWidth = 1;
 
 // Subtract (2 * pixelBorderWidth) as there is a border on each side.
-let width =  (contWidth / number) - (2 * pixelBorderWidth);
-let height = (contHeight / number) - (2 * pixelBorderWidth);
-console.log(`pixel width = ${width}`);
-console.log(`pixel height = ${height}`);
+let pixelWidth =  (contWidth / number) - (2 * pixelBorderWidth);
+let pixelHeight = (contHeight / number) - (2 * pixelBorderWidth);
+console.log(`pixel width = ${pixelWidth}`);
+console.log(`pixel height = ${pixelHeight}`);
 
 
-// Create new empty row to add pixels to.
+// Create new empty row <div> to add pixels to.
 for (let j = 0; j < number; j++) {
   pixelRow[j] = document.createElement('div');
   pixelRow[j].style.display = 'flex'; 
   for (let i = 0; i < number; i++) {
-    // Create 1 row's worth of pixels and append to row
+    // Create 1 row's worth of pixels and append to row <div>
     pixel[i] = document.createElement('div');
-    pixel[i].style.cssText = `width: ${width}px; height: ${height}px; border: solid ${pixelBorderWidth}px gray;`;
+    pixel[i].classList.add('pixel');
+    pixel[i].style.cssText = `width: ${pixelWidth}px; height: ${pixelHeight}px;`;
     pixelRow[j].appendChild(pixel[i]);  
   } 
   row.appendChild(pixelRow[j]);
@@ -45,7 +45,22 @@ container.appendChild(row);
 // ADJUST PIXEL COLOR WHEN MOUSE HOVERS OVER
 
 /*
+Create DOM list of listeners for each pixel
+Use to create listeners for each pixel
+
 Create a class for each color gradient
 Create listener event for each pixel
 When pixel is hovered over, change color.
 */ 
+
+const pixels = document.querySelectorAll('.pixel');
+
+function pixelBlack() {
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseenter', ()=> {
+      pixel.classList.add('pixelBlack');
+    });
+  });
+};
+
+pixelBlack();
