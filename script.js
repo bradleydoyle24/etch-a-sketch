fadc4d09ab739b64288d6bba8d0b95761844bbc8
Bradley <bradleydoyle24@gmail.com>
2023-06-16 21:19:17 -0400

    Create function call so user input creates pixels
    Changed entire program to a function to create pixels.  Currently
    takes user input to create pixels the first time, but will append
    to the old pixels with each new use.  Color changing functions no
    longer work.
    
    Re work so that each step is an individual function that is called
    by the master function.  Need to consider arguments, returns, etc...

--- script.js
+++ script.js
@@ -5,43 +5,43 @@ const row = document.createElement('div');
 row.style.display = 'flex';
 row.style.flexDirection = 'column';
 
-let number = 50;
 let pixel = [];
 let pixelRow = [];
 
-// Get width/height of container. clientWidth/height does not include the 
-// container border in calculations.
-let contWidth = container.clientWidth;
-let contHeight = container.clientHeight;
-console.log(`container width = ${contWidth}`);
-console.log(`container height = ${contHeight}`);
+function buildEtch(pixelNumber) {
+  // Get width/height of container. clientWidth/height does not include the 
+  // container border in calculations.
+  let contWidth = container.clientWidth;
+  let contHeight = container.clientHeight;
+  console.log(`container width = ${contWidth}`);
+  console.log(`container height = ${contHeight}`);
 
-// Allows for variable border widths for each pixel.
-let pixelBorderWidth = 1;
+  // Allows for variable border widths for each pixel.
+  let pixelBorderWidth = 1;
 
-// Subtract (2 * pixelBorderWidth) as there is a border on each side.
-let pixelWidth =  (contWidth / number) - (2 * pixelBorderWidth);
-let pixelHeight = (contHeight / number) - (2 * pixelBorderWidth);
-console.log(`pixel width = ${pixelWidth}`);
-console.log(`pixel height = ${pixelHeight}`);
+  // Subtract (2 * pixelBorderWidth) as there is a border on each side.
+  let pixelWidth =  (contWidth / pixelNumber) - (2 * pixelBorderWidth);
+  let pixelHeight = (contHeight / pixelNumber) - (2 * pixelBorderWidth);
+  console.log(`pixel width = ${pixelWidth}`);
+  console.log(`pixel height = ${pixelHeight}`);
 
 
-// Create new empty row <div> to add pixels to.
-for (let j = 0; j < number; j++) {
-  pixelRow[j] = document.createElement('div');
-  pixelRow[j].style.display = 'flex'; 
-  for (let i = 0; i < number; i++) {
-    // Create 1 row's worth of pixels and append to row <div>
-    pixel[i] = document.createElement('div');
-    pixel[i].classList.add('pixel');
-    pixel[i].style.cssText = `width: ${pixelWidth}px; height: ${pixelHeight}px;`;
-    pixelRow[j].appendChild(pixel[i]);  
-  } 
-  row.appendChild(pixelRow[j]);
+  // Create new empty row <div> to add pixels to.
+  for (let j = 0; j < pixelNumber; j++) {
+    pixelRow[j] = document.createElement('div');
+    pixelRow[j].style.display = 'flex'; 
+    for (let i = 0; i < pixelNumber; i++) {
+      // Create 1 row's worth of pixels and append to row <div>
+      pixel[i] = document.createElement('div');
+      pixel[i].classList.add('pixel');
+      pixel[i].style.cssText = `width: ${pixelWidth}px; height: ${pixelHeight}px;`;
+      pixelRow[j].appendChild(pixel[i]);  
+    } 
+    row.appendChild(pixelRow[j]);
+  }
+
+  container.appendChild(row);
 }
-
-container.appendChild(row);
-
 // ADJUST PIXEL COLOR WHEN MOUSE HOVERS OVER
 
 // Create DOM list of divs with .pixel class
@@ -57,6 +57,7 @@ function pixelBlack() {
   });
 };
 
+// PIXEL COLOR CHANGING FUNCTIONS
 function pixelRed() {
   pixels.forEach((pixel) => {
     pixel.addEventListener('mouseenter', ()=> {
@@ -103,12 +104,20 @@ Change color for buttons
 Adjust stylesheet
 */
 
-
-/* 
-CHANGE COLOR FOR BUTTONS
-
-Create a listener for each button selection
-When clicked, clear all color select functions
-Erase board?
-Set new color function
+/*
+Create constant for input class 'pixel-input' 
+Create event listener for input.
+Make value return to a variable.
+Change pixel number to variable.
+Start with a default number?
 */
+
+const pixelInputNumber = document.querySelector('#pixel-input');
+const pixelInputButton = document.querySelector('#pixel-input-button');
+
+pixelInputButton.addEventListener('click', () => {
+  console.log(pixelInputNumber.value);
+  let pixelNumber = pixelInputNumber.value;
+  buildEtch(pixelNumber);
+});
+
